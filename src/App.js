3083c909ec16652737cc9d18b9a8fd74030c1481
Aziz Boudi <azicode4@gmail.com>
2019-12-13 16:10:15 +0100

    add dompurify and marked

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React , {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("placeholder");
+
+  const handleTextAreaChange = event => {
+    setText(event.target.value)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <Editor handleTextAreaChange={handleTextAreaChange}/>
+      <Preview text={text}/>
     </div>
-  );
+    )
+}
+
+const Editor = (props) => {
+  return (
+    <textarea
+        id="editor"
+        style={{ width: "60%", marginBottom: "20px" }}
+        rows="27"
+        cols="120"
+        value={props.text}
+        onChange={props.handleTextAreaChange}
+      />
+   )
+}
+
+const Preview = (props) => {
+  return (
+    <div id="preview" style={{ background: 'cadetblue', width: "80%", height:"700px"}}>
+      {props.text}
+    </div>
+  )
 }
 
 export default App;
